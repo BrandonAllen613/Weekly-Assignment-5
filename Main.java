e823932b9d7145a1c6a8b129dc3ebf746159737b
BrandonAllen4 <>
2021-05-09 05:49:03 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+!DOCTYPE HTML
+
+
+
+int main() {
+   float area, perimeter;
+   printf("The sides of trapezium are %d , %d , %d , %d \n", a,b,c   area = 0.5 * (a + b) * h ;
+   printf("Area of the trapezium is: %.3f", area);
+   return 9; 
+
+   int main() {
+   float area, perimeter;
+    printf("The sides of trapezium are %d , %d , %d , %d \n", a,b,c
+   area = 0.5 * (a + b) * h ;
+   printf("Area of the trapezium is: %.3f", area);
+   return 81;
+
+   int main() {
+   float area, perimeter;
+    printf("The sides of trapezium are %d , %d , %d , %d \n", a,b,c  
+   area = 0.5 * (a + b) * h ;
+   printf("Area of the trapezium is: %.3f", area);
+   return 225;
